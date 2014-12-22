272993ccdbaef606333de144fd09154b030174e2
Mike Eng <mike@buildium.com>
2014-12-22 16:14:53 -0500

    not working - attempt to load natural width and height of images

--- js/card.js
+++ js/card.js
@@ -5,3 +5,46 @@ var s = skrollr.init({
  
 // Refresh Skrollr after resizing our sections
 s.refresh($('.homeSlide'));
+
+$( document ).ready(function() {
+	var polyhedron = new Image();
+	polyhedron.src = 'img/polyhedron.png';
+	var pw;
+	var ph;
+	//set natural width and natural height once the image is loaded
+	if (polyhedron.addEventListener){
+		polyhedron.addEventListener('load', function(){
+			pw = polyhedron.naturalWidth;
+			ph = polyhedron.naturalHeight;
+		}, false);
+	} else if (polyhedron.attachEvent){
+		polyhedron.attachEvent('onload', function(){
+			pw = polyhedron.naturalWidth;
+			ph = polyhedron.naturalHeight;
+		});
+	}
+
+	var triangle = new Image();
+	triangle.src = 'img/triangle.png';
+	var tw;
+	var th;
+	//set natural width and natural height once the image is loaded
+	if (triangle.addEventListener){
+		triangle.addEventListener('load', function(){
+			tw = triangle.naturalWidth;
+			th = triangle.naturalHeight;
+		}, false);
+	} else if (triangle.attachEvent){
+		triangle.attachEvent('onload', function(){
+			tw = triangle.naturalWidth;
+			th = triangle.naturalHeight;
+		});
+	}
+
+	console.log(tw);
+
+	function getRandomInt (min, max) {
+	    return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+//end document ready	
+});
